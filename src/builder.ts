fe91897cccb07852e22d1a8e873015d3814a538a
komed3 <webmaster@komed3.de>
2026-06-26 23:51:31 +0200

    add JSDoc comment

--- src/builder.ts
+++ src/builder.ts
@@ -5,7 +5,7 @@
 
 import { CRON_DEFAULTS } from './const';
 import { CronParser } from './parser';
-import type { CronFieldName, CronObject, SpecialAlias } from './types';
+import type { CronFieldName, CronObject, CronTuple, SpecialAlias } from './types';
 
 /** Immutable fluent builder for cron expressions. */
 export class CronBuilder {
@@ -14,9 +14,21 @@ export class CronBuilder {
   private readonly fields: CronObject;
   private readonly current?: CronFieldName;
 
-  /** Create a builder instance from a optional cron expression. */
-  public static create ( expr?: string | SpecialAlias | CronObject ) : CronBuilder {
-    return new CronBuilder( expr ? typeof expr === 'string' ? CronBuilder.parser.toObject( expr ) : expr : CRON_DEFAULTS );
+  /**
+   * Create a builder instance from a optional cron expression.
+   * 
+   * @param expr - Optional standard 5-field cron string, a partial cron object or special alias.
+   * @returns The current cron builder instance.
+   * 
+   * @example
+   * CronBuilder.create()
+   * CronBuilder.create( '0 2 0 JAN MON-FRI' )
+   * CronBuilder.create( { minute: '30', hour: '1' } )
+   * CronBuilder.create( '@daily' )
+   */
+  public static create ( expr?: string | SpecialAlias | Partial< CronObject > ) : CronBuilder {
+    return new CronBuilder( expr ? typeof expr === 'string' ? CronBuilder.parser.toObject( expr )
+      : { ...CRON_DEFAULTS, ...expr } : CRON_DEFAULTS );
   }
 
   private constructor ( fields: CronObject, current?: CronFieldName ) {
