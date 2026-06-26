addfa06342a2d0b4b0946c9669a02bd1e480caf5
komed3 <webmaster@komed3.de>
2026-06-26 23:36:46 +0200

    add JSDocs

--- src/builder.ts
+++ src/builder.ts
@@ -1,3 +1,8 @@
+/**
+ * CronBuilder
+ * Builds cron expressions using a immutable fluent design.
+ */
+
 import { CRON_DEFAULTS } from './const';
 import { CronParser } from './parser';
 import type { CronFieldName, CronObject, SpecialAlias } from './types';
@@ -9,6 +14,7 @@ export class CronBuilder {
   private readonly fields: CronObject;
   private readonly current?: CronFieldName;
 
+  /** Create a builder instance from a optional cron expression. */
   public static create ( expr?: string | SpecialAlias | CronObject ) : CronBuilder {
     return new CronBuilder( expr ? typeof expr === 'string' ? CronBuilder.parser.toObject( expr ) : expr : CRON_DEFAULTS );
   }
