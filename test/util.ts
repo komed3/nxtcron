6e99ca9c016acc9ecf43cfb93e29a218beddf79b
komed3 <webmaster@komed3.de>
2026-06-27 13:56:37 +0200

    log performance

--- test/util.ts
+++ test/util.ts
@@ -7,16 +7,17 @@ let passed = 0, failed = 0;
 
 /** Register and execute a test case. */
 export function test ( name: string, fn: () => void ) : void {
-  try {
-    fn();
-    passed++;
+  const t = performance.now();
 
+  try {
+    fn(), passed++;
     console.log( `✓ ${ name }` );
   } catch ( err ) {
     failed++;
-
     console.error( `✗ ${ name }` );
     console.error( err );
+  } finally {
+    console.log(`✓ ${ name } (${ ( performance.now() - t ).toFixed( 2 ) } ms)` );
   }
 }
 
