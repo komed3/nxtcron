b752c58ced6804a25bff4003d1ef6f58ba5adf4e
komed3 <webmaster@komed3.de>
2026-06-27 14:21:56 +0200

    write parser tests

---
+++ test/parser.test.ts
@@ -0,0 +1,30 @@
+import { expect, summary, test } from './util';
+import { parse, toObject, toTuple, validate } from '../src';
+
+console.log( '=== PARSER ===' );
+
+test( 'validate valid expression', () => {
+  expect( validate( '*/5 2 * JAN MON-FRI' ) ).toBe( true );
+} );
+
+test( 'validate invalid expression', () => {
+  expect( validate( '70 * * * *' ) ).toBe( false );
+} );
+
+test( 'parse wildcard', () => {
+  const expr = parse( '* * * * *' );
+
+  expect( expr.fields.minute.sorted.length ).toBe( 60 );
+  expect( expr.fields.hour.sorted.length ).toBe( 24 );
+} );
+
+test( 'toObject', () => {
+  expect( toObject( '5 12 * JAN MON' ) ).toEqual( {
+    minute: '5', hour: '12', dayOfMonth: '*', month: 'JAN', dayOfWeek: 'MON' } );
+} );
+
+test( 'toTuple', () => {
+  expect( toTuple( '5 12 * JAN MON' ) ).toEqual( [ '5', '12', '*', 'JAN', 'MON' ] );
+} );
+
+summary();
