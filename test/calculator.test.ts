fa2052f1992aeb53a01f21d143520f2b0e07eeb6
komed3 <webmaster@komed3.de>
2026-06-27 14:57:31 +0200

    test prev / next minute

--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -1,5 +1,17 @@
-import { expect, summary, hl, test } from './util';
 import { next, prev } from '../src';
+import { expect, hl, summary, test } from './util';
 
 const after = ( iso: string ) => ( { after: new Date( iso ) } );
 const before = ( iso: string ) => ( { before: new Date( iso ) } );
+
+hl( 'CALCULATOR' );
+
+test( 'next minute', () => {
+  expect( next( '* * * * *', after( '2026-06-26T18:32:11Z' ) )[ 0 ].toISOString() ).toBe( '2026-06-26T18:33:00.000Z' );
+} );
+
+test( 'prev minute', () => {
+  expect( prev( '* * * * *', before( '2026-06-26T18:32:11Z' ) )[ 0 ].toISOString() ).toBe( '2026-06-26T18:31:00.000Z' );
+} );
+
+summary();
