82bf8463f68eaf777c2724232fd1d36be924ab5c
komed3 <webmaster@komed3.de>
2026-06-27 14:58:39 +0200

    +3 tests

--- test/calculator.test.ts
+++ test/calculator.test.ts
@@ -14,4 +14,16 @@ test( 'prev minute', () => {
   expect( prev( '* * * * *', before( '2026-06-26T18:32:11Z' ) )[ 0 ].toISOString() ).toBe( '2026-06-26T18:31:00.000Z' );
 } );
 
+test( 'next step', () => {
+  expect( next( '*/15 * * * *', after( '2026-06-26T18:17:00Z' ) )[ 0 ].toISOString() ).toBe( '2026-06-26T18:30:00.000Z' );
+} );
+
+test( 'hour overflow', () => {
+  expect( next( '0 * * * *', after( '2026-06-26T18:59:30Z' ) )[ 0 ].toISOString() ).toBe( '2026-06-26T19:00:00.000Z' );
+} );
+
+test( 'day overflow', () => {
+  expect( next( '0 0 * * *', after( '2026-06-26T23:59:59Z' ) )[ 0 ].toISOString() ).toBe( '2026-06-27T00:00:00.000Z' );
+} );
+
 summary();
